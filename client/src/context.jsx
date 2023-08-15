4b90d2926dc02f4dbaba7ddf77ccd4b02b28b40a
chivicoko <victor.c.okoye@gmail.com>
2023-08-15 17:15:39 +0100

    Add axios to fetch api data...

--- client/src/context.jsx
+++ client/src/context.jsx
@@ -1,10 +1,25 @@
 import React, {useContext, useState} from "react";
 import { useEffect } from "react";
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
     const [cartNum, setCartNum] = useState(JSON.parse(localStorage.getItem('cart') || 0));
+    
+    const productsUrl = 'https://dummyjson.com/products/';
+    const fetchProducts = async (url) => {
+        try {
+            const {data} = await axios(url);
+            console.log(data);
+        } catch (error) {
+            console.log(error.response);
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts(productsUrl);
+    }, []);
 
     const cartUpdate = () => {
         setCartNum(cartNum + 1);
